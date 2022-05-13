dc4ec32403027f844420abb5dae2f12b62d71e0a
Débora Amaral <87581131+debooramaral@users.noreply.github.com>
2022-05-13 07:55:18 -0400

    Add files via upload

---
+++ gridjs.js
@@ -0,0 +1,19 @@
+var img1, img2;
+
+function troca(img1, img2){
+    alert("TROQUE");
+    img2 = document.getElementsByClassName("img1");
+
+    img2.innerHTML = img1
+
+    mudarimagem()
+}
+
+function mudarimagem(){
+    for(var img2){
+        img2.onclick = function (){
+            
+        }
+    }
+
+}
